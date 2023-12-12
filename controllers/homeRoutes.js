a9ec24838ef84be1610c820d291547e8489fe0d3
mono <traineralaina@gmail.com>
2023-12-12 13:37:15 -0700

    fixed issues

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,26 +2,34 @@ const router = require('express').Router();
 const { Posts, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-
-router.get('/dashboard', withAuth, async (req, res) => {
+// router.get('/', (req, res) => {
+//   res.render('homepage', { 
+//   }); 
+// });
+router.get('/dashboard', async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Posts }],
+
+    const postData = await Posts.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
     });
 
-    const user = userData.get({ plain: true });
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('dashboard', {
-      ...user,
-      logged_in: true
+    // Pass serialized data and session flag into template
+    res.render('dashboard', { 
+      posts, 
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 router.get('/', async (req, res) => {
   try {
@@ -70,6 +78,25 @@ router.get('/posts/:id', async (req, res) => {
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Posts }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
