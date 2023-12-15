350ffed86d8e8417cdb06596785526e74b4b3b70
mono <traineralaina@gmail.com>
2023-12-14 18:03:28 -0700

    still trying to get this deployed

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,21 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  // When using JAWSDB_URL, it's a complete URL so no additional parameters are needed
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // For local or custom database, pass the options as an object
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      port: 3306
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: process.env.DB_DIALECT,
+      // Additional options can be added here as needed
     }
   );
 }
 
 module.exports = sequelize;
-
